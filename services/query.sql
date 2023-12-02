bdf70145f7ddee38195261308f45651f14103394
Imom111 <imom11ivan@gmail.com>
2023-12-02 01:12:27 -0600

    test for deploy

--- services/query.sql
+++
@@ -1,8 +0,0 @@
-DROP DATABASE IF EXISTS db_thunder_house;
-CREATE DATABASE db_thunder_house;
-USE db_thunder_house;
-
--- ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'admin';
-
--- flush privileges;
-
